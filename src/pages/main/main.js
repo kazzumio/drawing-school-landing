28d1f0b42e2dd8c5ea0f23593a39ece74b3678c3
theManAndHisShadow <justemailpostbox@gmail.com>
2026-01-28 19:12:15 +0500

    [UI]: added faq accordion logic

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-    debugLog('success', 'Main page is successfully loaded');
+$(document).ready(function () {
+
+  $('.faq-toggle').on('click', function () {
+    const $item = $(this).closest('.faq-item');
+
+    $('.faq-item.active').not($item).removeClass('active');
+
+    $item.toggleClass('active');
+  });
+  debugLog('success', 'Accordion switcher is successfully loaded');
+
+
+  debugLog('success', 'Main page is successfully loaded');
 });
